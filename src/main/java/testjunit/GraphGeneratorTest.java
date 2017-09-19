c0908e39a4fc923ed91ff1addb982ebd36644eae
pmk kamara <pmkamara@esp.sn>
2017-09-18 20:52:56 -0400

    Project completed

--- src/main/java/testjunit/GraphGeneratorTest.java
+++ src/main/java/testjunit/GraphGeneratorTest.java
@@ -1,8 +1,7 @@
 package testjunit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.princeton.cs.algs4.Graph;
@@ -14,67 +13,90 @@ public class GraphGeneratorTest {
 	@Test
 	public void testBinaryTree() {
 		Graph g = GraphGenerator.binaryTree(2);
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "binaryTree", g.getTypeName());
-
+		assertEquals("Test de type", "binaryTree", g.getTypeName());
 	}
 
 	@Test
 	public void testBipartite() {
 		Graph g = GraphGenerator.bipartite(4, 3, 0.2);
-		
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "bipartite", g.getTypeName());
-
+		assertEquals("Test de type", "bipartite", g.getTypeName());
 	}
 	
 	@Test
 	public void testBipartite2() {
 		Graph g = GraphGenerator.bipartite(6, 2, 3);
-		
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "bipartite", g.getTypeName());
-
+		assertEquals("Test de type", "bipartite", g.getTypeName());
 	}
-	
-	
+
 	@Test
 	public void testComplete() {
 		Graph g = GraphGenerator.complete(8);
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "complete", g.getTypeName());
+		assertEquals("Test de type", "complete", g.getTypeName());
 	}
 
 	@Test
 	public void testCompleteBipartite() {
 		Graph g = GraphGenerator.completeBipartite(3,5);
-		
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "completeBipartite", g.getTypeName());
+		assertEquals("Test de type", "completeBipartite", g.getTypeName());
 	}
 	
 	@Test
 	public void testCycle() {
 		Graph g = GraphGenerator.cycle(8);
-		
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "cycle", g.getTypeName());
+		assertEquals("Test de type", "cycle", g.getTypeName());
 	}
 	
 	@Test
 	public void testEulerianCycle() {
 		Graph g = GraphGenerator.eulerianCycle(8, 3);
-		
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "eulerianCycle", g.getTypeName());
+		assertEquals("Test de type", "eulerianCycle", g.getTypeName());
 	}
 	
 	@Test
 	public void testEulerianPath() {
 		Graph g = GraphGenerator.eulerianPath(6,2);
-		
-		//assertEquals("Test de type", "", g.getTypeName());
-		assertEquals("Test de type",  "eulerianPath", g.getTypeName());
+		assertEquals("Test de type", "eulerianPath", g.getTypeName());
+	}
+	
+	@Test
+	public void testSimpleIntInt() {
+		Graph g = GraphGenerator.simple(3,2);
+		assertEquals("Test de type", "simple", g.getTypeName());
 	}
 
+	@Test
+	public void testSimpleIntDouble() {
+		Graph g = GraphGenerator.simple(5, 0.5);
+		assertEquals("Test de type", "simple", g.getTypeName());
+	}
+
+	@Test
+	public void testPath() {
+		Graph g = GraphGenerator.path(4);
+		assertEquals("test de type", "path", g.getTypeName());
+	}
+
+	@Test
+	public void testWheel() {
+		Graph g = GraphGenerator.wheel(5);
+		assertEquals("test de type", "wheel", g.getTypeName());
+	}
+
+	@Test
+	public void testStar() {
+		Graph g = GraphGenerator.star(3);
+		assertEquals("test de type", "star", g.getTypeName());
+	}
+
+	@Test
+	public void testRegular() {
+		Graph g = GraphGenerator.regular(5, 2);
+		assertEquals("test de type", "regular", g.getTypeName());
+	}
+
+	@Test
+	public void testTree() {
+		Graph g = GraphGenerator.tree(3);
+		assertEquals("test de type", "tree", g.getTypeName());
+	}
 }
--- src/main/java/testjunit/GraphGeneratorTestVerticeCount.java
+++ src/main/java/testjunit/GraphGeneratorTestVerticeCount.java
@@ -1,6 +1,6 @@
 package testjunit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -12,59 +12,90 @@ public class GraphGeneratorTestVerticeCount {
 	@Test
 	public void testBinaryTree() {
 		Graph g = GraphGenerator.binaryTree(2);
-
-		assertEquals("Test de nombbre de sommets", 2, g.V());
+		assertEquals("Test du nombre de sommets pour BinaryTree", 2, g.V());
 	}
 
 	@Test
 	public void testBipartite() {
 		Graph g = GraphGenerator.bipartite(4, 3, 0.2);
-
-		assertEquals("Test de nombbre de sommets", 7, g.V());
+		assertEquals("Test du nombre de sommets pour Bipartite", 7, g.V());
 	}
 	
 	@Test
 	public void testBipartite2() {
 		Graph g = GraphGenerator.bipartite(6, 2, 3);
-
-		assertEquals("Test de nombbre de sommets", 8, g.V());
+		assertEquals("Test du nombre de sommets pour Bipartite2", 8, g.V());
 	}
-	
-	
+
 	@Test
 	public void testComplete() {
 		Graph g = GraphGenerator.complete(8);
-
-		assertEquals("Test de nombbre de sommets", 8, g.V());
+		assertEquals("Test du nombre de sommets pour Complete", 8, g.V());
 	}
 
 	@Test
 	public void testCompleteBipartite() {
 		Graph g = GraphGenerator.completeBipartite(3,5);
-
-		assertEquals("Test de nombbre de sommets", 8, g.V());
+		assertEquals("Test du nombre de sommets pour Complete Bipartite ", 8, g.V());
 	}
 	
 	@Test
 	public void testCycle() {
 		Graph g = GraphGenerator.cycle(8);
-
-		assertEquals("Test de nombbre de sommets", 8, g.V());
+		assertEquals("Test du nombre de sommets pour Cycle", 8, g.V());
 	}
 	
 	@Test
 	public void testEulerianCycle() {
 		Graph g = GraphGenerator.eulerianCycle(8, 3);
-
-		assertEquals("Test de nombbre de sommets", 8, g.V());
+		assertEquals("Test du nombre de sommets pour Euleriean Cycle", 8, g.V());
 	}
 	
 	@Test
 	public void testEulerianPath() {
 		Graph g = GraphGenerator.eulerianPath(6,2);
-
-		assertEquals("Test de nombbre de sommets", 6, g.V());
+		assertEquals("Test du nombre de sommets pour Eulerian Path", 6, g.V());
 	}
 
+	@Test
+	public void testSimpleIntInt() {
+		Graph g = GraphGenerator.simple(3,2);
+		assertEquals("Test du nombre de sommets pour Simple (Int, Int)", 3, g.V());
+	}
 
+	@Test
+	public void testSimpleIntDouble() {
+		Graph g = GraphGenerator.simple(5, 0.5);
+		assertEquals("Test du nombre de sommets pour Simple (Double) ",5, g.V());
+	}
+
+	@Test
+	public void testPath() {
+		Graph g = GraphGenerator.path(4);
+		assertEquals("Test du nombre de sommets pour Path", 4, g.V());
+	}
+
+	@Test
+	public void testWheel() {
+		Graph g = GraphGenerator.wheel(5);
+		assertEquals("Test du nombre de sommets pour Wheel", 5, g.V());
+	}
+
+	@Test
+	public void testStar() {
+		Graph g = GraphGenerator.star(3);
+		assertEquals("Test du nombre de sommets pour Star",3, g.V());
+	}
+
+	@Test
+	public void testRegular() {
+		Graph g = GraphGenerator.regular(5, 2);
+		assertEquals("Test du nombre de sommets pour Regular",5 , g.V());
+	}
+
+	@Test
+	public void testTree() {
+		Graph g = GraphGenerator.tree(3);
+		assertEquals("Test du nombre de sommets pour Tree",3,  g.V());
+	}
 }
