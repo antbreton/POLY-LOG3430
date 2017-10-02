e1af1548c06f1eb48e59f8cab2de4b9fb5fb20b9
Antoine Breton <antoine.breton06@gmail.com>
2017-10-02 11:34:19 -0400

    4 Premiers tests TP2

---
+++ src/main/java/testjunit/AllTestsTP2.java
@@ -0,0 +1,14 @@
+package testjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ GraphGeneratorECTests.class})
+
+public class AllTestsTP2 {}
