e192f4ee771f69c26ab2446495df2ea1a65df2c0
Antoine Breton <antoine.breton06@gmail.com>
2017-11-20 10:23:31 -0500

    First attributes

--- src/main/java/testjunit/MadumTest.java
+++ src/main/java/testjunit/MadumTest.java
@@ -40,11 +40,29 @@ public class MadumTest {
 	 */
 	@Test
 	public void secondTranchForN() {
+		
+		Queue<String> q = new Queue<>();
+		
+			
+		assertEquals(q.size(),0);
+		
+		try
+		{
+			q.dequeue();
+			fail("Queue is empty");
+		} catch(Exception e) {}		
+		
+		assertEquals(q.size(),0);
+		
+		q.enqueue("s1");
+		
+		assertEquals(q.size(),1);
+
 	}
 	
 	/*
 	 * Test the first tranch of FIRST attribute
-	 * Queue -> peek -> enqueue -> peek -> dequeue -> peek
+	 * Queue -> peek -> enqueue -> peek -> dequeue -> peek -> isEmpty -> iterator
 	 */
 	@Test
 	public void firstTranchForFirst() {
@@ -67,13 +85,52 @@ public class MadumTest {
 			q.peek();
 			fail("Cannot peek on an empty Queue");
 		} catch(Exception e) {}		
+		
+		q.isEmpty();
+		
+		q.iterator();
+		
+		try
+		{
+			q.peek();
+			fail("Cannot peek on an empty Queue");
+		} catch(Exception e) {}			
 	}
 	
 	/*
 	 * Test the second tranch of FIRST attribute
-	 * Queue -> peek -> dequeue -> peek -> enqueue -> peek
+	 * Queue -> peek -> dequeue -> peek -> enqueue -> peek -> isEmpty -> iterator
 	 */
 	@Test
 	public void secondTranchForFirst() {
+		Queue<String> q = new Queue<>();
+		
+		try
+		{
+			q.peek();
+			fail("Cannot peek on an empty Queue");
+		} catch(Exception e) {}
+		
+		try
+		{
+			q.dequeue();
+			fail("Queue is empty");
+		} catch(Exception e) {}		
+		
+		try
+		{
+			q.peek();
+			fail("Cannot peek on an empty Queue");
+		} catch(Exception e) {}		
+		
+		q.enqueue("s1");
+		
+		assertEquals("s1",q.peek());
+	
+		q.isEmpty();
+		
+		q.iterator();
+		
+		assertEquals("s1",q.peek());
 	}
 }
